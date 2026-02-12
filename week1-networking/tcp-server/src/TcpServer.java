4dac182e750f9533ee7101196a50c4581368fd4e
karansahani78 <119734907+karansahani78@users.noreply.github.com>
2026-02-12 17:19:59 +0545

    Standardize project structure with src folder

--- week1-networking/tcp-server/src/TcpServer.java
+++ week1-networking/tcp-server/src/TcpServer.java
@@ -1,5 +1,5 @@
 public class TcpServer {
     public static void main(String[] args) {
-        System.out.println("Tcp server starting.....");
+        System.out.println("Tcp server is starting.....");
     }
 }
